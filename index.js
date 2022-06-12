2336329e9bf12016acb7db454ba5bf84372364bd
iAmmez <38929253+iAmmez@users.noreply.github.com>
2022-06-12 10:56:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const myWorksBtn = document.getElementById("my-works-btn")
 
 myWorksBtn.addEventListener("click",function(){
 	console.log("clicked")
-	window.open("MY_WORKS\\Cyptopy\\Cyptopy.html")
+	window.open("MY_WORKS\\Cryptopy\\index.html")
 })
